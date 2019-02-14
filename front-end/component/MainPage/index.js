17b3692ba30125396dfb089b559508c67bd0cabc
Freddy <freddy.jd2@gmail.com>
2019-02-14 12:14:37 -0500

    Fixed styling

--- front-end/component/MainPage/index.js
+++ front-end/component/MainPage/index.js
@@ -23,6 +23,23 @@ class HeadContainer extends Component {
             </div>
           </div>
         </section>
+
+      <section className="fdb-block">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-12 col-md-6 mb-4 mb-md-0">
+      
+            <img alt="image" className="img-fluid" src="/static/assets/img/smile.svg" />
+          </div>
+          <div className="col-12 col-md-6 col-lg-5 ml-md-auto text-left">
+            <h1>Froala Blocks</h1>
+            <p className="lead">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+            <p><a className="btn btn-secondary mt-4" href="https://www.froala.com">Download</a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
       </>
     );
   }
